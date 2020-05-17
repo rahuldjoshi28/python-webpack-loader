ebd6357284bc26328e9403baf9cc71e1bc22a4c2
rahuldjoshi28 <rahuldjoshi28@gmail.com>
2020-05-17 18:35:14 +0530

    Add uglification of result

--- integrationTests/integrations.test.js
+++ integrationTests/integrations.test.js
@@ -1,6 +1,9 @@
 const fs = require('fs')
 const path = require('path')
 const loader = require('../loader')
+const terser = require('terser')
+
+jest.mock('terser')
 
 function pythonAsText(testName) {
 	return fs.readFileSync(path.join(__dirname, 'python', testName, 'test.py'))
@@ -13,11 +16,34 @@ function pythonPath(testName) {
 const tests = ['simple', 'functions', 'class', 'modules']
 
 describe('integrations', () => {
-	it.each(tests)('should work for %s python code', (testName) => {
+	beforeEach(() => {
+		jest.clearAllMocks()
+	})
+
+	it.each(tests)(
+		'should work for %s python code and non uglified',
+		(testName) => {
+			this.context = pythonPath(testName)
+			const pythonCode = pythonAsText(testName)
+			const loaderWithMockThis = loader.bind(this)
+			process.env.UGLIFY = 'false'
+			expect(loaderWithMockThis(pythonCode)).toMatchSnapshot()
+		}
+	)
+
+	it.each(tests)('should work for %s python code and uglified', (testName) => {
 		this.context = pythonPath(testName)
 		const pythonCode = pythonAsText(testName)
 		const loaderWithMockThis = loader.bind(this)
 
-		expect(loaderWithMockThis(pythonCode)).toMatchSnapshot()
+		process.env.UGLIFY = 'false'
+		const nonUglifiedCode = loaderWithMockThis(pythonCode)
+
+		expect(terser.minify).not.toHaveBeenCalled()
+
+		process.env.UGLIFY = 'true'
+		loaderWithMockThis(pythonCode)
+
+		expect(terser.minify).toHaveBeenLastCalledWith(nonUglifiedCode)
 	})
 })
--- loader.js
+++ loader.js
@@ -1,8 +1,14 @@
 const transpile = require('./src/transpile')
+const Terser = require('terser')
 
 module.exports = function pythonLoader(source) {
 	const directoryPath = this.context
-	const sourceCopy = source.toString()
+	const sourceCopy = JSON.stringify(source.toString())
+	const uglify = process.env.UGLIFY == 'true'
+	let code = transpile(sourceCopy, directoryPath)
 
-	return transpile(sourceCopy, directoryPath)
+	if (uglify) {
+		code = new Terser.minify(code).code
+	}
+	return code
 }
